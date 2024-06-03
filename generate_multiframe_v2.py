801bfcc611cc91d91af0eacdae69b6bfd674694a
parkie0517 <parkie0517@gmail.com>
2024-06-03 19:07:28 +0900

    fuse multiscan complete

--- generate_multiframe_v2.py
+++ generate_multiframe_v2.py
@@ -2,6 +2,8 @@
     This file 'generate_multiframe_v2.py' is used to create multiframe semantic KITTI dataset.
     Created by Heejun Park :)
 """
+
+
 import numpy as np
 from numpy.linalg import inv
 import argparse
@@ -21,65 +23,6 @@ def count_files(directory):
     
     return file_count
 
-def get_data_all(file_base, dataset_path):
-    """
-    Description
-    This Function is used to read the point cloud and voxel data
-    ---------- ---------- ---------- ---------- ---------- 
-    Input
-    file_base: the name of the data we want to read (eg. 000000)
-    dataset_path: location of the original dataset
-    ---------- ---------- ---------- ---------- ---------- 
-    Below are the main steps
-    1. Define each of the file's location
-    2. Read each files independently
-    3. Return the read data
-    ---------- ---------- ---------- ---------- ---------- 
-    Output
-    i_pc, i_label, i_invalid, i_occluded 
-    """
-
-    # Function used to read and reshape point cloud data
-    def load_pc_data(file_path):
-        with open(file_path, 'rb') as file:
-            data = np.fromfile(file, dtype=np.float32) # Point cloud data are stored as 4 byte float type 
-            point_cloud_data = np.reshape(data, (-1, 4))
-            return point_cloud_data
-
-
-    # Function used to read and reshape binary voxel data
-    def load_binary_data(file_path):
-        with open(file_path, 'rb') as file:
-            data = np.fromfile(file, dtype=np.uint8)  # Read data as 8-bit unsigned integers
-            bits = np.unpackbits(data)  # Convert bytes to bits
-            return bits.reshape((256, 256, 32))  # Reshape to 3D array
-
-
-    # Function used to read and reshape label voxel
-    def load_label_data(file_path):
-        # Open the file in binary mode
-        with open(file_path, 'rb') as file:
-            # Read the file content into a numpy array of type uint16
-            data = np.fromfile(file, dtype=np.uint16)
-        
-        return data.reshape((256, 256, 32))
-
-
-    # build file path
-    pc_path = os.path.join(dataset_path, "velodyne", f"{file_base}.bin")
-    label_path = os.path.join(dataset_path, "voxels", f"{file_base}.label")
-    invalid_path = os.path.join(dataset_path, "voxels", f"{file_base}.invalid")
-    occluded_path = os.path.join(dataset_path, "voxels", f"{file_base}.occluded")
-    
-    
-    # 2. read the data
-    pc_data = load_pc_data(pc_path)
-    label_data = load_label_data(label_path)
-    invalid_data = load_binary_data(invalid_path)
-    occluded_data = load_binary_data(occluded_path)
-
-    # 3. return the data
-    return pc_data, label_data, invalid_data, occluded_data
 
 def get_pc(file_base, dataset_path):
     """
@@ -99,6 +42,7 @@ def get_pc(file_base, dataset_path):
     i_pc
     """
 
+
     # Function used to read and reshape point cloud data
     def load_pc_data(file_path):
         with open(file_path, 'rb') as file:
@@ -115,6 +59,7 @@ def get_pc(file_base, dataset_path):
 
     # 3. return the data
     return pc_data
+
 
 def load_poses(poses_path, Tr, Tr_inv):
     """
@@ -163,127 +108,6 @@ def load_calib(calib_path):
     return matrix
 
 
-def align_filter_add_binary_data_optimized(i_bin, j_bin, i_pose, j_pose):
-    """
-    Description
-    This function is used to add j into i
-    ---------- ---------- ---------- ---------- ---------- 
-    Input
-    i_bin: i's bin file
-    j_bin: j's bin file
-    i_pose: i's pose information
-    j_pose: j's pose information
-    ---------- ---------- ---------- ---------- ---------- 
-    Below are the main steps
-    1. Transform j into i's coordinate frame
-    2. Filter (only convert j-th voxels that will be inside the i-th coordinate frame)
-    3. Add the filtered voxels to i_bin
-    ---------- ---------- ---------- ---------- ---------- 
-    Output
-    i_bin + j_bin
-    """
-    
-    """ 1. Transform """
-    # Get the indices of non-zero voxels in j_bin
-    z, y, x = np.nonzero(j_bin)
-    ones = np.ones_like(x)
-    j_coords = np.vstack((x, y, z, ones)).T
-    # Transform the coordinates from j's frame to i's frame
-    new_points = (j_coords @ j_pose.T)[:, :3]
-    new_points = new_points - i_pose[:3, 3]
-    new_coords = (new_points @ i_pose[:3, :3].T).astype(int)
-    
-
-    """ 2. Filter """
-    # Extract x, y, z coordinates
-    x_coords = new_coords[:, 0]
-    y_coords = new_coords[:, 1]
-    z_coords = new_coords[:, 2]
-
-    # Create boolean masks based on boundary conditions
-    x_mask = (0 <= x_coords) & (x_coords < i_bin.shape[0])
-    y_mask = (0 <= y_coords) & (y_coords < i_bin.shape[1])
-    z_mask = (0 <= z_coords) & (z_coords < i_bin.shape[2])
-
-    # Combine the masks to filter out coordinates outside the boundaries
-    valid_mask = x_mask & y_mask & z_mask
-
-    # Apply the mask to filter quantized_coords
-    filtered_coords = new_coords[valid_mask]
-
-    """ 3. Add """
-    i_bin[filtered_coords[:, 0], filtered_coords[:, 1], filtered_coords[:, 2]] = 1
-
-    return i_bin
-
-
-
-def align_filter_add_binary_data(i_bin, j_bin, i_pose, j_pose):
-    """
-    Description
-    This function is used to add j into i
-    ---------- ---------- ---------- ---------- ---------- 
-    Input
-    i_bin: i's bin file
-    j_bin: j's bin file
-    i_pose: i's pose information
-    j_pose: j's pose information
-    ---------- ---------- ---------- ---------- ---------- 
-    Below are the main steps
-    1. Transform j into i's coordinate frame
-    2. Filter (only convert j-th voxels that will be inside the i-th coordinate frame)
-    3. Add the filtered voxels to i_bin
-    ---------- ---------- ---------- ---------- ---------- 
-    Output
-    i_bin + j_bin
-    """
-    
-    """ 1. Transform """
-    coords_list = []
-    # repeat this process for all the individual voxels
-    for z in range(j_bin.shape[2]): # = range(0, 32)
-        for y in range(j_bin.shape[1]): # = range(0, 256)
-            for x in range(j_bin.shape[0]): # = range(0, 256)
-                if j_bin[x, y, z] == 1:
-                    voxel_coords = np.array([x, y, z, 1])
-                    coords_list.append(voxel_coords)
-
-
-    j_coords = np.vstack(coords_list)
-    new_points = np.sum(np.expand_dims(j_coords, 2) * j_pose.T, axis=1)
-    new_points = new_points[:, :3] 
-    new_coords = new_points - i_pose[:3, 3]
-    new_coords = np.sum(np.expand_dims(new_coords, 2) * i_pose[:3, :3], axis=1)
-    quantized_coords = np.round(new_coords).astype(int) # Quantize the coordinates
-
-
-    """ 2. Filter """
-    # Extract x, y, z coordinates
-    x_coords = quantized_coords[:, 0]
-    y_coords = quantized_coords[:, 1]
-    z_coords = quantized_coords[:, 2]
-
-    # Create boolean masks based on boundary conditions
-    x_mask = (0 <= x_coords) & (x_coords < i_bin.shape[0])
-    y_mask = (0 <= y_coords) & (y_coords < i_bin.shape[1])
-    z_mask = (0 <= z_coords) & (z_coords < i_bin.shape[2])
-
-    # Combine the masks to filter out coordinates outside the boundaries
-    valid_mask = x_mask & y_mask & z_mask
-
-    # Apply the mask to filter quantized_coords
-    filtered_coords = quantized_coords[valid_mask, :]
-
-    
-    """ 3. Add """
-    for coord in filtered_coords:
-        x, y, z = coord
-        i_bin[x, y, z] = 1
-    
-
-    return i_bin
-
-
 def fuse_multiscan(i_pc, j_pc, i_pose, j_pose):
     """
     ---------- ---------- ---------- ---------- ---------- 
@@ -299,10 +123,9 @@ def fuse_multiscan(i_pc, j_pc, i_pose, j_pose):
     Below are the main steps
     1. Fuse multiscan using the pose information
     2. Filter out point cloud that are outside the boundary
-    3. Voxelize
     ---------- ---------- ---------- ---------- ---------- 
     Output
-    fused i + j in voxel format
+    fused i + j in point cloud format
     ---------- ---------- ---------- ---------- ---------- 
     """
     
@@ -320,11 +143,22 @@ def fuse_multiscan(i_pc, j_pc, i_pose, j_pose):
     # 1.3. Fuse i_pc and transformed j_pc
     fused_pc = np.concatenate((i_pc, new_coords), 0)
 
-    """ 2. Filter Point Cloud """
-    
 
 
-    return i_bin
+    return fused_pc
+
+
+"""
+    Global Variables
+"""
+max_volume_space=[51.2, 25.6, 4.4]
+min_volume_space=[0, -25.6, -2]
+
+max_bound = np.asarray(max_volume_space)
+min_bound = np.asarray(min_volume_space)
+
+
+
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -444,7 +278,7 @@ if __name__ == '__main__':
         shutil.copy(os.path.join(dataset, "voxels", f"{i_file_base}.label"), output_dir)
         shutil.copy(os.path.join(dataset, "voxels", f"{i_file_base}.invalid"), output_dir)
         shutil.copy(os.path.join(dataset, "voxels", f"{i_file_base}.occluded"), output_dir)
-        
+    
 
         # Repeat to fuse differenet lidar scans
         for j in range(i + increment, i + increment * n, increment):
@@ -453,11 +287,11 @@ if __name__ == '__main__':
             j_file_base = f"{j:06d}" # Convert j's data type from INT to STR and pad 0 at the front
             j_pc = get_pc(j_file_base, dataset) # read j-th point cloud data
             j_pose = poses[j] # read j-th pose
+ 
 
-
-            i_bin = fuse_multiscan(i_pc, j_pc, i_pose, j_pose)
-        
+            i_pc = fuse_multiscan(i_pc, j_pc, i_pose, j_pose)
         # Save fused scan
+        
         np.packbits(i_bin).tofile(os.path.join(output_dir, f"{i_file_base}.bin"))
         exit(0)
 
